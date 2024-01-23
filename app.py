cd1aa0b22ce113019d750adea5cb92d12f4e1571
allamurodxakimov <xakimov2806@gmail.com>
2024-01-23 13:33:08 +0300

    smartphone

--- app.py
+++ app.py
@@ -7,52 +7,58 @@ db = SmartphoneDB('db.json')
 
 
 ## view all smartphone
+@app.route('/')
+def Home():
+    return "Hello students, I have phone"
+
 @app.route('/smartphones', methods=['GET'])
 def get_all_smartphones():
     """Returns all smartphones in the database"""
-    pass
+    return db.all_smartphone()
 
 
 # view all brands
 @app.route('/smartphones/brands', methods=['GET'])
 def get_all_brands():
     """Returns all brands in the database"""
-    pass
+    return db.brands()
 
 
 # view all smartphones by brand
 @app.route('/smartphones/<brand>', methods=['GET'])
 def get_smartphone_by_brand(brand):
     """Returns all products by brand"""
-    pass
+    return db.get_smartphone_by_brand(brand)
 
 
 # view smartphone by name
 @app.route('/smartphones/name/<name>', methods=['GET'])
 def get_smartphone_by_name(name):
     """Returns a product by name"""
-    pass
+    return db.get_smartphone_by_name(name)
 
 
 # view smartphone by price
 @app.route('/smartphones/price/<price>', methods=['GET'])
 def get_smartphone_by_price(price):
     """Returns a product by price"""
-    pass
-
+    return db.get_smartphone_by_price(price)
 
 # view add smartphone
 @app.route('/smartphone/add', methods=['POST'])
 def add_smartphone():
     """Adds a product to the database"""
-    pass
+    if request.method=="POST":
+        smartpone = request.get_json()
+    brand = "Apple"
+    return db.add_smartphone(brand=brand, smartphone=smartpone)
 
 
 # view delete smartphone
-@app.route('/smartphone/delete/<doc_id>', methods=['DELETE'])
-def delete_smartphone(doc_id):
+@app.route('/smartphone/delete/<brand>/<int:doc_id>', methods=['DELETE'])
+def delete_smartphone(brand, doc_id):
     """Deletes a product from the database"""
-    pass
+    return db.delete_smartphone(brand=brand, doc_id=doc_id)
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,32 +1,78 @@
 from tinydb import TinyDB, Query
+from tinydb.database import Document
 
 
 class SmartphoneDB:
     def __init__(self, db_path):
-        self.db = TinyDB(db_path)
+        self.db = TinyDB(db_path, indent=4, separators=(',', ': '))
         self.query = Query()
     
+    def all_smartphone(self):
+        brands_all = ["Huawei","Redmi","Vivo","Nokia","Apple","Oppo","Samsung"]
+        smartphones = []
+        for brand in brands_all:
+            brand_all = self.db.table(brand).all()
+            smartphones.append(brand_all)
+        return smartphones
+
     def brands(self):
         """Returns all brands in the database"""
-        pass
+        brand = self.db.tables()
+        return list(brand)
     
     def get_smartphone_by_brand(self, brand):
         """Returns all products by brand"""
-        pass
+        data = self.db.table(brand)
+        return data.all()
     
     def get_smartphone_by_name(self, name):
         """Returns a product by name"""
-        pass
-
+        brands_all = ["Huawei","Redmi","Vivo","Nokia","Apple","Oppo","Samsung"]
+        smartpone = []
+        for brand in brands_all:
+            data = self.db.table(brand).search(self.query.name==name)
+            if data!=[]:
+                smartpone.append(data)
+        return smartpone[0]
+    
     def get_smartphone_by_price(self, price):
         """Returns a product by price"""
-        pass
+        brands_all = ["Huawei","Redmi","Vivo","Nokia","Apple","Oppo","Samsung"]
+        smartpone = []
+        for brand in brands_all:
+            data = self.db.table(brand).search(self.query.price == price)
+            if data != []:
+                smartpone.append(data)
+        return smartpone
     
     def add_smartphone(self, smartphone, brand):
         """Adds a product to the database"""
-        pass
+        phone = self.db.table(brand)
+        phone.insert(smartphone)
+        return {"statust":"ok"}
     
     def delete_smartphone(self, doc_id, brand):
         """Deletes a product from the database"""
-        pass
+        phone = self.db.table(brand)
+        phone.remove(doc_ids=[doc_id])
+        return {"statust":"ok"}
     
+if __name__=="__main__":
+    db = SmartphoneDB('db.json')
+
+    # print(db.all_smartphone())
+    # print(db.brands())
+    # print(db.get_smartphone_by_brand('Nokia'))
+    # print(db.get_smartphone_by_name('Apple iPhone 6s'))
+    # print(db.get_smartphone_by_price('Apple', 851.6))
+    # smartphone = {
+    #         "name":"Apple i13Pro",
+    #         "company":"Apple",
+    #         "color": "Blue",
+    #         "RAM": "8GB",
+    #         "memory": "128GB",
+    #         "price":13400.3,
+    #         "img_url":"https://images-na.ssl-images-amazon.com/images/I/51qBzX0pGYL._SL1000_.jpg"
+    #     }
+    # print(db.add_smartphone(brand="Apple", smartphone=smartphone))
+    # print(db.delete_smartphone(brand="Apple", doc_id=30))
